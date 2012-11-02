f09393766b1240eb3c02ddde92059697621b2293
Chris Browne <yoda2031@gmail.com>
2012-11-02 22:19:56 +0100

    Corrected instruction length of opcode 31C0 to 2 from 3

--- include/instruction.h
+++ include/instruction.h
@@ -8,7 +8,7 @@ typedef uint8_t opcode_t;
 // a useful macro
 #define SET_OPCODE(dest,src) { dest.op = src >> 2; dest.d = (src & 0x2) >> 1; dest.w = src & 0x1; }
 
-extern inline unsigned int instruction_length(uint8_t opcode)
+static inline unsigned int instruction_length(uint8_t opcode)
 {
     unsigned int rv = 0;
     switch(opcode)
@@ -17,7 +17,7 @@ extern inline unsigned int instruction_length(uint8_t opcode)
             rv = 2;
             break;
         case 0x31: 
-            rv = 3;
+            rv = 2;
             break;
     }
     return rv;
