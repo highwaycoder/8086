df43607f05191582e85b2a59b58e405a5f5d6b19
Chris Browne <yoda2031@gmail.com>
2012-11-02 22:19:05 +0100

    Fix for opcode 0x31C0 which is actually supposed to be XOR ax,ax

--- bitwise.c
+++ bitwise.c
@@ -18,7 +18,10 @@ void xor_modrm_reg16(cpu_t* cpu,uint8_t modrm,uint16_t src)
 	switch(modrm)
 	{
 		case 0xC0:
-			xor_reg16_reg16(&cpu->ax,src);
+		// apparently 0x31C0 is XOR ax,ax which makes sense but doesn't 
+		// agree with what I read in the Intel Software Developer's 
+		// Manual...
+			xor_reg16_reg16(&cpu->ax,cpu->ax);
 			break;
 	}
 }
